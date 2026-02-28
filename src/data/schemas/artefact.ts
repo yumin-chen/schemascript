9d42d878079f0cb08662810814b623e15e843d42
Yumin Chen <yumin@chen.so>
2026-02-28 08:29:17 +0000

    Update artefact.ts

--- src/data/schemas/artefact.ts
+++ src/data/schemas/artefact.ts
@@ -2,8 +2,46 @@ import type { SchemaBuilder } from "../core";
 import { field, Schema, Table, value } from "../core";
 
 const artefact: SchemaBuilder = () => ({
-	name: field.text("name"),
-	timestamp: field.integer("timestamp").default(value.now),
+
+	/**
+	 * The full path including filename of the artefact. 
+	 *
+	 * @name pathname
+	 * @description The full path including filename of the artefact. 
+	 * @type TEXT
+	 * @unique
+	 */
+	pathname: field.text("pathname").unique(),
+
+
+	/**
+	 * The cryptographic hash digest of the artefact content. 
+	 *
+	 * @name digest
+	 * @description The cryptographic hash digest of the artefact content.
+	 * @type TEXT
+	 */
+	digest: field.text("digest").unique(),
+
+	/**
+	 * The last modification timestamp of the artefact. 
+	 *
+	 * @name modified_at
+	 * @description The last modification timestamp of the artefact.
+	 * @type INTEGER.Timestamp
+	 * @derived
+	 */
+	modifiedAt: field.integer("modified_at", { mode: "timestamp" }).default(value.now),
+
+	/**
+	 * The creation timestamp of the artefact. 
+	 *
+	 * @name created_at
+	 * @description The creation timestamp of the artefact.
+	 * @type INTEGER.Timestamp
+	 * @derived
+	 */
+	createdAt: field.integer("created_at", { mode: "timestamp" }).default(value.now),
 });
 
 const artefactSchema = Schema("Artefact", artefact);
